61ddb4cbba7eed93c9d4273a81f4f9f28db7a0f3
Jheimys Bassani <jheimysbb@gmail.com>
2023-06-19 15:16:04 +0100

    :construction: Adicionando evento de click nos botões

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 (function () {
 
-    //-----------------------  SELEÇÃO DE ELEMENTOS --------------------------------------------------
+    //-----------------------  SELEÇÃO DE ELEMENTOS ----------------------------------------------------------------
 
     //Form for entering tasks 
     const todoForm = document.querySelector('#todo-form')
@@ -16,16 +16,62 @@
 
 
     // ----------------------------    FUNCTIONS ------------------------------------------------------------------
+    
+    const saveTodo = (texto) => {
+
+        const todo = document.createElement('div')
+        todo.classList.add('todo')
+
+        const todoTitle = document.createElement('h3')
+        todoTitle.innerText = texto
+        todo.appendChild(todoTitle)
+
+        const doneBtn = document.createElement('button')
+        doneBtn.classList.add('finish-todo')
+        doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
+        todo.appendChild(doneBtn)
+
+        const editBtn = document.createElement('button')
+        editBtn.classList.add('edit-todo')
+        editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
+        todo.appendChild(editBtn)
+
+        const deletBtn = document.createElement('button')
+        deletBtn.classList.add('remove-todo')
+        deletBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+        todo.appendChild(deletBtn)
+
+        todoList.appendChild(todo)
+
+        todoInput.value = ''
+        todoInput.focus()
+
+
+    }
 
     // ---------------------------  EVENTS ------------------------------------------------------------------------
+    
     todoForm.addEventListener('submit', (e) => {
         e.preventDefault()
         
         const inputValue = todoInput.value
 
         if(inputValue) {
-            console.log(inputValue)
+            saveTodo(inputValue)
         }
     })
-    
+
+    document.addEventListener('click', (e) => {
+        const targetEl = e.target
+        const parentEl = targetEl.closest('div')
+        
+        if(targetEl.classList.contains('finish-todo')) {
+            parentEl.classList.toggle('done')
+        }
+
+        if(targetEl.classList.contains('remove-todo')) {
+            parentEl.remove()
+        }
+    })
+
 })()
